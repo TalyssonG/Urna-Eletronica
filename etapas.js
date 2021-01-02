10466b731e47e8a3be23a9876aefad4f3ff9796e
TalyssonG <lopestalysson99@gmail.com>
2021-01-02 15:13:31 -0300

    iniciando a lógica

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numeros: 38111,
+                nome: 'Petrysson',
+                partido: 'Abc',
+                fotos: [
+                    { url: 'vereador1.jpg', legenda: 'vereador', small: false }
+                ]
+            },
+
+            {
+                numeros: 77222,
+                nome: 'Lívia',
+                partido: 'dfr',
+                fotos: [
+                    { url: 'vereadora2.jpg', legenda: 'vereadora' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Talysson',
+                partido: 'Abc',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'prefeito1.jpg', legenda: 'Prefeito' },
+                    { url: 'vice-prefeito1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Jessica',
+                partido: 'hnf',
+                vice: 'Marcos',
+                fotos: [
+                    { url: 'prefeita2.jpg', legenda: 'Prefeita' },
+                    { url: 'vice-prefeito2.jpg', legenda: 'Vice-Prefeito', small: true },
+                ]
+            },
+        ]
+    }
+];
+
---
+++ script.js
@@ -0,0 +1,2 @@
+//Iniciando a lógica
+let seuVotoPara = document.querySelect('.d-1-1 span')
